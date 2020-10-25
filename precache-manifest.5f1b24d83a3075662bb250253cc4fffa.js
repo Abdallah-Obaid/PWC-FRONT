6b950e351f6bf2d0833668d4099b9ffcdaecaee2
Abdallah <eng.abdallahobaid@gmail.com>
2020-10-25 09:29:06 +0300

    Updates

---
+++ precache-manifest.5f1b24d83a3075662bb250253cc4fffa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82552da78ebd7f237f7886398ae8524c",
+    "url": "/PWC-FRONT/index.html"
+  },
+  {
+    "revision": "2165b110ff505d1301f1",
+    "url": "/PWC-FRONT/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "02a09fc2ad6cdfc969f4",
+    "url": "/PWC-FRONT/static/css/main.4879ca17.chunk.css"
+  },
+  {
+    "revision": "2165b110ff505d1301f1",
+    "url": "/PWC-FRONT/static/js/2.d19e4162.chunk.js"
+  },
+  {
+    "revision": "7417019f59899a1b20fd49e68b03ed49",
+    "url": "/PWC-FRONT/static/js/2.d19e4162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a09fc2ad6cdfc969f4",
+    "url": "/PWC-FRONT/static/js/main.e75fd8d2.chunk.js"
+  },
+  {
+    "revision": "79457027f04aa7c1e19a",
+    "url": "/PWC-FRONT/static/js/runtime-main.8bd8e2c5.js"
+  },
+  {
+    "revision": "08731c8b107ec1c382076385e3657db4",
+    "url": "/PWC-FRONT/static/media/new2.08731c8b.png"
+  }
+]);
